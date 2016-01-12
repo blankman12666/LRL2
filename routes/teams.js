fae71b5ff0fdcae9acea23ebdebecaa083173f49
Kyle Shrader <kyle.shrader@outlook.com>
2016-01-12 14:52:23 -0800

    Update teams.js

--- routes/teams.js
+++ routes/teams.js
@@ -1,5 +1,10 @@
 var router = require('express').Router();
 
+router.param(':team_name', function(req, res, next, team_name) {
+  req.team_name = team_name;
+  next();
+});
+
 /* GET /teams/:team_name - shows team page */
 router.get('/teams/:team_name', function(req, res) {
   res.render('teams/' + team_name);
